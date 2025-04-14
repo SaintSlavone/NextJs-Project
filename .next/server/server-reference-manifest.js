3e416ce87fa44eefe0c003fb30cc622fb761a85f
Viacheslav <viakostee@gmail.com>
2025-04-15 01:53:12 +0200

    route change

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RxFRVEs40cHBTNucNqC10WE4OLtvmJI9MJwavB+pKOw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
